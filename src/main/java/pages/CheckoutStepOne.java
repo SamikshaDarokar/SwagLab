879f7b22b2714043474d5d0d39d0987517237879
Samiksha <Samiksha@LAPTOP-MJA9L9M3>
2025-03-22 17:25:28 +0530

    This is session

--- src/main/java/pages/CheckoutStepOne.java
+++ src/main/java/pages/CheckoutStepOne.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-
 import base.TestBase;
 import utility.ReadData;
 
